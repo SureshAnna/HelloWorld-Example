daf223650a4897b13e8c94fae06a6dcdbd81e2b4
sureshanna <suresh.kanaparthi13@gmail.com>
2018-06-12 15:30:47 +0530

    Code

--- src/main/java/HelloSuresh.java
+++ src/main/java/HelloSuresh.java
@@ -2,6 +2,6 @@
 public class HelloSuresh {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to automation testing, Mr. Suresh Kanaparthi......");
+		System.out.println("Welcome to automation testing, Mr. Rajesh KothaKota......");
 	}
 }
