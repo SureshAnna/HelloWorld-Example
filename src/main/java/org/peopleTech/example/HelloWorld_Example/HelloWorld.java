30e324cda8a544d917ab98067d1c7c007fe26189
sureshanna <suresh.kanaparthi13@gmail.com>
2018-06-12 13:02:41 +0530

    pom + class

---
+++ src/main/java/org/peopleTech/example/HelloWorld_Example/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.peopleTech.example.HelloWorld_Example;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("++++++++++CICT++++++++++++");
+	}
+}
