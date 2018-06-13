db2301f0c00b012a4dacf4e5899603a9aed8fedc
sureshanna <suresh.kanaparthi13@gmail.com>
2018-06-13 17:13:06 +0530

    Cocde

--- src/main/java/HelloSuresh.java
+++ src/main/java/HelloSuresh.java
@@ -1,9 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class HelloSuresh {
 
 	public static void main(String[] args)
 	{
 		System.out.println("Welcome to automation testing......Mr................");
+		
+		WebDriver driver;
+		
+		driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+		 String URL = "http://www.dhtmlx.com/docs/products/dhtmlxTree/index.shtml";
+
+		 driver.get(URL);
 	}
 	 
 	
