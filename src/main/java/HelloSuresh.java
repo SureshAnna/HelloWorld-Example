0c97ecada31c35b6ffb667ea878cf20347090431
sureshanna <suresh.kanaparthi13@gmail.com>
2018-06-13 12:21:41 +0530

    Code

--- src/main/java/HelloSuresh.java
+++ src/main/java/HelloSuresh.java
@@ -1,13 +1,21 @@
+import java.sql.Driver;
 
-
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 
 public class HelloSuresh {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to automation testing........Mr..........");
+		System.out.println("Welcome to automation testing..................");
 	}
 	 
 	
