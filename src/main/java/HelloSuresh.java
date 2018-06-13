c32f81f5b01f2241fc06438de8b88b295b938fc0
sureshanna <suresh.kanaparthi13@gmail.com>
2018-06-13 12:18:54 +0530

    Code

--- src/main/java/HelloSuresh.java
+++ src/main/java/HelloSuresh.java
@@ -7,7 +7,7 @@ public class HelloSuresh {
 
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to automation testing..................");
+		System.out.println("Welcome to automation testing........Mr..........");
 	}
 	 
 	
