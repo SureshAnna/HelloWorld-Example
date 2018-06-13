16c51568d1129e3e4b7ef3d6828a2a65d0cd544d
sureshanna <suresh.kanaparthi13@gmail.com>
2018-06-13 12:32:37 +0530

    Code

--- src/main/java/HelloSuresh.java
+++ src/main/java/HelloSuresh.java
@@ -1,15 +1,3 @@
-import java.sql.Driver;
-
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
-
 
 public class HelloSuresh {
 
