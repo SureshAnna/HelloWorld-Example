5dc74a29cd21ec03a58fc01520816ea2009eaefe
sureshanna <suresh.kanaparthi13@gmail.com>
2018-06-13 17:58:36 +0530

    code

--- src/main/java/org/peopleTech/example/HelloWorld_Example/HelloSuresh.java
+++ src/main/java/org/peopleTech/example/HelloWorld_Example/HelloSuresh.java
@@ -1,10 +1,24 @@
 package org.peopleTech.example.HelloWorld_Example;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 
 public class HelloSuresh {
+	
+	@Test
+	public void mainMethod()
+	{
+		System.out.println("Welcome to automation testing......Mr................");
+		
+		WebDriver driver;
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Suresh\\workSpace4Automation\\BasicPractice\\chromedriver.exe");
+		driver=new ChromeDriver();
+		 driver.manage().window().maximize();
+		 String URL = "http://www.dhtmlx.com/docs/products/dhtmlxTree/index.shtml";
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		 driver.get(URL);
 	}
-
+	 
+	
 }
