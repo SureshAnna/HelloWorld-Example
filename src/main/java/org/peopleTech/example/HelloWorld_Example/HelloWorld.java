453b6e8477fa5a8376ec2923150bd9703b0abb9d
sureshanna <suresh.kanaparthi13@gmail.com>
2018-06-13 14:04:40 +0530

    test.xml change

--- src/main/java/org/peopleTech/example/HelloWorld_Example/HelloWorld.java
+++ src/main/java/org/peopleTech/example/HelloWorld_Example/HelloWorld.java
@@ -1,8 +1,14 @@
 package org.peopleTech.example.HelloWorld_Example;
 
-public class HelloWorld {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
-	public static void main(String[] args) {
+public class HelloWorld {
+	
+	@Test
+	public void test()
+	{
 		System.out.println("++++++++++CICT++++++++++++");
+		Assert.assertEquals("100", "100");
 	}
 }
