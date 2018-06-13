dea2dba4de43ead20018aa02c3d5e26d2a941b84
sureshanna <suresh.kanaparthi13@gmail.com>
2018-06-13 17:18:28 +0530

    Updated code

--- src/main/java/HelloSuresh.java
+++ src/main/java/HelloSuresh.java
@@ -9,7 +9,7 @@ public class HelloSuresh {
 		System.out.println("Welcome to automation testing......Mr................");
 		
 		WebDriver driver;
-		
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Suresh\\workSpace4Automation\\BasicPractice\\chromedriver.exe");
 		driver=new ChromeDriver();
 		 driver.manage().window().maximize();
 		 String URL = "http://www.dhtmlx.com/docs/products/dhtmlxTree/index.shtml";
